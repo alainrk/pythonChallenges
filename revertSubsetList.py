2ca2079e26f010372cf4c1f445fc9f0e724674d6
alainrk <alain.narko@gmail.com>
2016-05-22 20:19:37 +0200

    bug reversed sublist

--- revertSubsetList.py
+++ revertSubsetList.py
@@ -1,11 +1,9 @@
 # Amazon recruiting test
 
-def revertSubsetList(l,n):
-    res = []
-    for i in range(1, n+1):
-        print i
+def revertSubsetList(l,n,res=[]):
+    for i in range(1, len(l)/n+1):
         for j in range(1,n+1):
             res += [ l[i*n-j] ]
     return res
 
-print revertSubsetList([1,2,3,4,5,6,7,8,9], 3) # [3, 2, 1, 6, 5, 4, 9, 8, 7]
+print revertSubsetList([1,2,3,4,5,6,7,8,9,10,11,12], 3)

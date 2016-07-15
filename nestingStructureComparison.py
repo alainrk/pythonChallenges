09c51c5339d09d20139d672e1f41eddd1279da78
alainrk <alain.narko@gmail.com>
2016-07-15 13:36:34 +0200

    Nesting struct

---
+++ nestingStructureComparison.py
@@ -0,0 +1,19 @@
+'''
+# should return True
+same_structure_as([ 1, 1, 1 ], [ 2, 2, 2 ] )
+same_structure_as([ 1, [ 1, 1 ] ], [ 2, [ 2, 2 ] ] )
+
+# should return False
+same_structure_as([ 1, [ 1, 1 ] ], [ [ 2, 2 ], 2 ] )
+same_structure_as([ 1, [ 1, 1 ] ], [ [ 2 ], 2 ] )
+
+# should return True
+same_structure_as([ [ [ ], [ ] ] ], [ [ [ ], [ ] ] ] )
+
+# should return False
+same_structure_as([ [ [ ], [ ] ] ], [ [ 1, 1 ] ] )
+'''
+
+def same_structure_as(a,b):
+    n = lambda l: [] if not(l) else list(map(lambda i: n(i) if isinstance(i, list) else 0, l))
+    return False if not isinstance(a, list) or not isinstance(b, list) else n(a) == n(b)

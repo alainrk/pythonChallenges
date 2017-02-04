4a21f80839e256d8514a6336cd667baea6d242eb
alainrk <alain.narko@gmail.com>
2017-02-04 07:13:55 +0100

    tries cleaned

--- tries.py
+++ tries.py
@@ -6,14 +6,6 @@ class node:
         if child is not None:
             self.child[child.value] = child
 
-    def find(self, value):
-        if value in self.child:
-            return self.child[value]
-        return None
-
-    def isComplete(self):
-        return len(self.child) == 0
-
     def addToken(self, token):
         if len(token) == 0:
             self.end = True
@@ -54,7 +46,19 @@ root = node("*", None)
 root.addToken("A")
 root.addToken("a")
 root.addToken("Vaffa")
-root.addToken("Vaffren")
+root.addToken("Vaffrensdfsdfedfsdfsdfssfs")
+root.addToken("Vaffrensdfefssfs")
+root.addToken("Vaffrensdfsdfsdfssfs")
+root.addToken("Vaffrensdfsdfsdxxfs")
+root.addToken("Vaffrensxxfssfs")
+root.addToken("Vaffrefsdfsdfssfs")
+root.addToken("Vaffrefsdfsdfssfsc")
+root.addToken("Vaffrefsdfsdfssfsd")
+root.addToken("Vaffrefsdfsdfssfse")
+root.addToken("Vaffrefsdfsdfssfsr")
+root.addToken("Vaffrefsdfsdfssfsgg")
+root.addToken("Vaffrefsdfsdfssfscc")
+root.addToken("Vaffrefsdfsdfssfsxxx")
 root.addToken("Vaffin")
 root.addToken("Vaffend")
 root.addToken("Bast")
@@ -62,10 +66,28 @@ root.addToken("Baste")
 root.addToken("Basto")
 root.addToken("Baston")
 root.addToken("Bastone")
+root.addToken("Bastone")
+root.addToken("Bastone")
+root.addToken("Bastone")
+root.addToken("Basqweqweqweqweqwetone")
+root.addToken("Basqweqweqweqweqwetone")
+root.addToken("Basqweqweqweqweqwetosdfsdfe")
+root.addToken("Bastone")
+root.addToken("Bastdasdasdasdne")
+root.addToken("Bastone")
+root.addToken("Bastone")
+root.addToken("Bastone")
+root.addToken("Bastone")
 root.addToken("Bastoner")
 root.addToken("Bastonerro")
 
 print root.countOccurrences("A")
 print root.countOccurrences("V")
+print root.countOccurrences("x")
+print root.countOccurrences("X")
+print root.countOccurrences("Vx")
+print root.countOccurrences("Ax")
+print root.countOccurrences("Basx")
+print root.countOccurrences("111")
 print root.countOccurrences("Bast")
-print root.countOccurrences("Bastone")
+print root.countOccurrences("Bas")

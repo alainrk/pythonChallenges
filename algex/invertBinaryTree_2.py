8376a532b501a9e6444b83525b16b6b154ebd642
Alain Di Chiappari <alain.devcs@gmail.com>
2021-05-21 05:50:20 +0200

    invert bin tree

---
+++ algex/invertBinaryTree_2.py
@@ -0,0 +1,19 @@
+from queue import Queue
+
+def invertBinaryTree(tree):
+    q = Queue()
+	q.put(tree)
+	while not q.empty():
+		curr = q.get()
+		if curr is None:
+			continue
+		curr.left, curr.right = curr.right, curr.left
+		q.put(curr.left)
+		q.put(curr.right)
+
+# This is the class of the input binary tree.
+class BinaryTree:
+    def __init__(self, value):
+        self.value = value
+        self.left = None
+        self.right = None

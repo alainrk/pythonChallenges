3a56b26d31ae56f0c177ad42604b4d2d81bbd462
Alain Di Chiappari <alain.devcs@gmail.com>
2021-05-25 09:35:33 +0200

    btree

---
+++ algex/binaryTreeFindSuccessor.py
@@ -0,0 +1,28 @@
+# This is an input class. Do not edit.
+class BinaryTree:
+    def __init__(self, value, left=None, right=None, parent=None):
+        self.value = value
+        self.left = left
+        self.right = right
+        self.parent = parent
+
+def findSuccessor(tree, node):
+	if node.right is not None:
+		return findLeftmost(node.right)
+	if node.parent is not None:
+		return findLeftyParent(node)
+	return None
+
+def findLeftyParent(node):
+	curr = node
+	while curr.parent is not None:
+		if curr.parent.left and curr.parent.left.value == curr.value:
+			return curr.parent
+		curr = curr.parent
+	return None
+
+def findLeftmost(node):
+	curr = node
+	while curr.left:
+		curr = curr.left
+	return curr

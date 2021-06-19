6885bfdc006f8f93b1940190be5bcf67f6a0dd8c
Alain Di Chiappari <alain.devcs@gmail.com>
2021-06-19 16:01:03 +0200

    algex

--- ctci/canCostruct.py
+++ ctci/canCostruct.py
@@ -3,13 +3,30 @@ def canConstruct(bank, word):
   res[0] = True
 
   '''
-  Bank = 
-  ['', a, ab, abc, abcd, abcde, abcdef]
-  [T,  F,  F,   F,    F,     F,      F]
+  Bank = ['ab', 'abc', 'cd', 'def', 'abcd']
+
+        ['', a, ab, abc, abcd, abcde, abcdef]
+  res = [T,  F,  T,   F,    F,     F,      F]
+         |   |   |
+         |_ab|   |
+         |_____ab|
   '''
   for i in range(len(res)):
-    for bw in bank:
-      res[i] = word == word[:i] + bw
+    if not res[i]:
+      continue
+    for j in range(i + 1, len(res)):
+      for bw in bank:
+        # print(f'\'{word[:j]}\'', '==', word[:i], '+', bw, f', res[{word[:j]}] = {word[:j] == word[:i] + bw}')
+        prefix = word[:i]
+        currPartial = word[:j]
+        itCan = currPartial == prefix + bw
+        res[j] = res[j] or itCan # can be optimized exiting
+  print(res)
   return res[len(word)]
 
+
 assert(canConstruct(['ab', 'abc', 'cd', 'def', 'abcd'], 'abcdef') == True)
+assert(canConstruct(['fizz', 'buzz', 'fizzbuzz'], 'fizzbuzz') == True)
+assert(canConstruct(['fizz', 'buzz'], 'fizzbuzz') == True)
+assert(canConstruct(['a', 'aa', 'aaa', 'aaaa', 'aaaaa'], 'aaaaaaaaaaaaaaaaaaaaa') == True)
+assert(canConstruct(['a', 'aa', 'aaa', 'aaaa', 'aaaaa'], 'aaaaaaaaaaaaaaaaaaaaab') == False)

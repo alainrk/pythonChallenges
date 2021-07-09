cb6e3eeca7bbf9307b0601c35e2cef2a4e88c223
Alain Di Chiappari <alain.devcs@gmail.com>
2021-07-09 13:50:24 +0200

    algex

--- algex/validStartingCityGas.py
+++ algex/validStartingCityGas.py
@@ -1,7 +1,7 @@
 def validStartingCity(distances, fuel, mpg):
 	mingas, minc = 0, 0
 	gas = 0
-    for i in range(1, len(distances)):
+	for i in range(1, len(distances)):
 		gas += fuel[i - 1] * mpg - distances[i-1]
 		if gas < mingas:
 			mingas, minc = gas, i
